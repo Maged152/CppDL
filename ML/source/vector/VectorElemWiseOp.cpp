40ed8f3fe91dc0b82efec5bda00d0070e6e07122
Ahmed Maged <mageda9000@gmail.com>
2023-10-08 23:30:55 +0300

    add cache lines calculation

--- ML/source/vector/VectorElemWiseOp.cpp
+++ ML/source/vector/VectorElemWiseOp.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <omp.h>
 #include <future>
+#include <new>
+#include <cmath>
 
 namespace qlm
 {
@@ -18,8 +20,10 @@ namespace qlm
 			return Status::INVALID_DIMENTIONS;
 		}
 
-		const unsigned int num_used_threads = pool.Size();
 		const unsigned int total_length = len;
+		const  int lines = std::ceil((float)len / std::hardware_destructive_interference_size);
+		const  int threads =  std::min(lines, pool.Size());
+		
 
 		auto op_vec = [](const float* const __restrict  src1, const float* const __restrict  src2, float* const __restrict  dst, const unsigned int size)
 		{
@@ -31,28 +35,24 @@ namespace qlm
 			}
 		};
 		// divide the matrix among the threads
-		const unsigned int thread_length = total_length / num_used_threads;
-		const unsigned int thread_tail_length = total_length % num_used_threads;
-		std::vector<std::future<void>> futures(num_used_threads);
+		const unsigned int thread_len = (lines / threads) * std::hardware_destructive_interference_size;
+		const unsigned int thread_tail = total_length - thread_len * threads;
+		std::vector<std::future<void>> futures(threads);
 		// launch the threads
 		int next_idx = 0;
 
 #pragma omp unroll full
-		for (unsigned int i = 0; i < thread_tail_length; i++)
+		for (unsigned int i = 0; i < threads - 1; i++)
 		{
-			futures[i] = pool.Submit(op_vec, &data[next_idx], &src.data[next_idx], &dst.data[next_idx], thread_length + 1);
-			next_idx += thread_length + 1;
+			futures[i] = pool.Submit(op_vec, &data[next_idx], &src.data[next_idx], &dst.data[next_idx], thread_len);
+			next_idx += thread_len;
 		}
+		// tail thread
+		futures[threads - 1] = pool.Submit(op_vec, &data[next_idx], &src.data[next_idx], &dst.data[next_idx], thread_len + thread_tail);
 
-#pragma omp unroll full
-		for (unsigned int i = thread_tail_length; i < num_used_threads; i++)
-		{
-			futures[i] = pool.Submit(op_vec, &data[next_idx], &src.data[next_idx], &dst.data[next_idx], thread_length);
-			next_idx += thread_length;
-		}
 		// wait for the threads to finish
 #pragma omp unroll full
-		for (unsigned int i = 0; i < num_used_threads; i++)
+		for (unsigned int i = 0; i < threads; i++)
 		{
 			futures[i].wait();
 		}
