51e2403f8fe86458bc5d97154da13233f01d09a2
Ahmed Maged <mageda9000@gmail.com>
2023-12-20 22:36:24 +0200

    start panda implementation

--- ML/include/panda.h
+++ ML/include/panda.h
@@ -1,14 +1,31 @@
 #pragma once
+#include "vector.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 namespace qlm
 {
-	class panda
+	class Panda
 	{
 	private:
+		int cols;
+		int rows;
+
+		std::vector<std::string> headers;
+		std::vector<std::string> str_data;
+		std::vector<qlm::Vector> num_data;
+	public:
+		Panda()
+		{}
+
+		~Panda();
 
 	public:
+		Status Read(const std::string& file_name, const std::string& sep);
+		Status Write(const std::string& file_name, const std::string& sep);
 
-	public:
-
+		void Print(int num_row = 10);
 	};
 }
